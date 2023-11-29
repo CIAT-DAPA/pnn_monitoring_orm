27707bda38628323942e0716bc8b787ce0a11788
Victor Hernandez <victor.993_@hotmail.com>
2023-11-29 17:10:22 -0500

    primer commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pnn_monitoring_orm",
+    version='0.0.0',
+    author="victor-993",
+    author_email="v.hernandez@cgiar.com",
+    description="ORM para la base de datos para la herramienta de monitoreo financiero PNN",
+    url="https://github.com/CIAT-DAPA/pnn_monitoring_orm",
+    download_url="https://github.com/CIAT-DAPA/pnn_monitoring_orm",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "greenlet==3.0.1",
+        "psycopg2==2.9.9",
+        "SQLAlchemy==2.0.23",
+        "typing_extensions==4.8.0",
+    ]
+)
